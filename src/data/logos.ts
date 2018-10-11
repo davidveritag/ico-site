981a09fe5cc8c2649f6671696c40172a63e19c97
kishan007 <kishansparkle007@gmail.com>
2018-10-12 01:33:59 +0530

    partners,clients and supports promotions

--- src/data/logos.ts
+++ src/data/logos.ts
@@ -2,131 +2,308 @@ export var logos = {
   "cornerstone" : [
     {
       'name'  : 'Top Fund',
-      'image' : '../assets/img/logos/topfund.png',
+      'image' : '../assets/img/logos/sg_0.png',
       'link'  : ''
     },
     {
-      'name'  : 'TAAS',
-      'image' : '../assets/img/logos/taas.png',
-      'link'  : 'https://taas.fund'
+      'name'  : 'sg_1',
+      'image' : '../assets/img/logos/sg_1.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'sg_2',
+      'image' : '../assets/img/logos/sg_2.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'sg_3',
+      'image' : '../assets/img/logos/sg_3.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'sg_4',
+      'image' : '../assets/img/logos/sg_4.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'sg_5',
+      'image' : '../assets/img/logos/sg_5.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'sg_6',
+      'image' : '../assets/img/logos/sg_6.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'sg_7',
+      'image' : '../assets/img/logos/sg_7.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'sg_8',
+      'image' : '../assets/img/logos/sg_8.png',
+      'link'  : ''
     }
   ],
   "partners" : [
     {
-      'name'  : 'Campflex',
-      'image' : '../assets/img/logos/campflex-logo.png',
+      'name'  : 'c_0',
+      'image' : '../assets/img/logos/c_0.png',
       'link'  : ''
     },
     {
-      'name'  : 'Proline',
-      'image' : '../assets/img/logos/proline_logo.png',
-      'link'  : 'https://www.pro-line.com.tr/en/SitePages/home.aspx'
+      'name'  : 'c_1',
+      'image' : '../assets/img/logos/c_1.png',
+      'link'  : ''
     },
     {
-      'name'  : 'Gazers',
-      'image' : '../assets/img/logos/logo.png',
-      'link'  : 'http://www.gazeru.co.jp/'
+      'name'  : 'c_2',
+      'image' : '../assets/img/logos/c_2.png',
+      'link'  : ''
     },
     {
-      'name'  : 'MW',
-      'image' : '../assets/img/logos/momentum_color.png',
-      'link'  : 'https://momentum.asia'
+      'name'  : 'c_3',
+      'image' : '../assets/img/logos/c_3.png',
+      'link'  : ''
     },
     {
-      'name'  : 'MagicQ',
-      'image' : '../assets/img/logos/MQ Studios Logo.png',
-      'link'  : 'http://www.mqstudios.tv'
+      'name'  : 'c_4',
+      'image' : '../assets/img/logos/c_4.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'c_5',
+      'image' : '../assets/img/logos/c_5.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'c_6',
+      'image' : '../assets/img/logos/c_6.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'c_7',
+      'image' : '../assets/img/logos/c_7.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'c_8',
+      'image' : '../assets/img/logos/c_8.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'c_9',
+      'image' : '../assets/img/logos/c_9.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'c_10',
+      'image' : '../assets/img/logos/c_10.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'c_11',
+      'image' : '../assets/img/logos/c_11.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'c_12',
+      'image' : '../assets/img/logos/c_12.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'c_13',
+      'image' : '../assets/img/logos/c_13.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'c_14',
+      'image' : '../assets/img/logos/c_14.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'c_15',
+      'image' : '../assets/img/logos/c_15.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'c_16',
+      'image' : '../assets/img/logos/c_16.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'c_17',
+      'image' : '../assets/img/logos/c_17.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'c_18',
+      'image' : '../assets/img/logos/c_18.png',
+      'link'  : ''
+    },
+    {
+      'name'  : 'c_19',
+      'image' : '../assets/img/logos/c_19.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_20',
+      'image' : '../assets/img/logos/c_20.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_21',
+      'image' : '../assets/img/logos/c_21.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_22',
+      'image' : '../assets/img/logos/c_22.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_23',
+      'image' : '../assets/img/logos/c_23.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_24',
+      'image' : '../assets/img/logos/c_24.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_25',
+      'image' : '../assets/img/logos/c_25.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_26',
+      'image' : '../assets/img/logos/c_26.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_27',
+      'image' : '../assets/img/logos/c_27.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_28',
+      'image' : '../assets/img/logos/c_28.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_29',
+      'image' : '../assets/img/logos/c_29.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_30',
+      'image' : '../assets/img/logos/c_30.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_31',
+      'image' : '../assets/img/logos/c_31.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_32',
+      'image' : '../assets/img/logos/c_32.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_33',
+      'image' : '../assets/img/logos/c_33.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_34',
+      'image' : '../assets/img/logos/c_34.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_35',
+      'image' : '../assets/img/logos/c_35.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_36',
+      'image' : '../assets/img/logos/c_36.png',
+      'link'  : ''
+    },{
+      'name'  : 'c_37',
+      'image' : '../assets/img/logos/c_37.png',
+      'link'  : ''
     }
   ],
   "supported" : [
     {
-      'name'  : 'Snap Innovations',
-      'image' : '../assets/img/logos/logo (2).png',
-      'link'  : 'http://www.snapinnovations.com/'
+      'name'  : 'Infocomm media',
+      'image' : '../assets/img/logos/infocomm.png',
+      'link'  : ''
     },
     {
-      'name'  : 'IPI',
-      'image' : '../assets/img/logos/IPI_MasterLogoColAW.png',
-      'link'  : 'https://www.ipi-singapore.org/'
+      'name'  : 'enterprises singapore',
+      'image' : '../assets/img/logos/es.png',
+      'link'  : ''
     },
-    {
-      'name'  : 'NEM',
-      'image' : '../assets/img/logos/download.png',
-      'link'  : 'https://nem.io/'
-    }
   ],
-  "member_of" : [
-    {
-      'name'  : 'SFA',
-      'image' : '../assets/img/logos/01_Logo_Original_Horizontal.png',
-      'link'  : 'https://singaporefintech.org/'
-    },
-    {
-      'name'  : 'ACCESS',
-      'image' : '../assets/img/logos/ACCESS-Crypto-Currency-Association-Singapore-1827259756.png',
-      'link'  : 'https://www.access-sg.org/'
-    }
-  ],
-  "featurings" : [
-    {
-      'name'  : 'Yahoo',
-      'image' : '../assets/img/logos/yahoo_logo_detail.png',
-      'link'  : 'https://sg.news.yahoo.com/blockchain-platform-startup-morpheus-labs-receives-angel-funding-082312233.html'
-    },
-    {
-      'name'  : 'e27',
-      'image' : '../assets/img/logos/e27-logo.png',
-      'link'  : 'https://e27.co/blockchain-startup-morpheus-labs-raises-funding-launches-ico-20180116/'
-    },
-    {
-      'name'  : 'Deal Street Asia',
-      'image' : '../assets/img/logos/dealstreet-720x720.png',
-      'link'  : 'https://www.dealstreetasia.com/stories/morpheus-labs-raises-funds-at-2m-valuation-25m-ico-90477/'
-    },
-    {
-      'name'  : 'Influencive',
-      'image' : '../assets/img/logos/influencive.png',
-      'link'  : 'https://www.influencive.com/morpheus-labs-potential-dominate-blockchain-era/'
-    },
-    {
-      'name'  : 'The Drum',
-      'image' : '../assets/img/logos/the_drum.png',
-      'link'  : 'http://www.thedrum.com/profile/news/281678/some-insight-morpheus-labs-potential-disrupt-the-blockchain-industry%201/30/178'
-    },
-    {
-      'name'  : '天天快报',
-      'image' : '../assets/img/logos/qq.png',
-      'link'  : 'https://kuaibao.qq.com/s/20180129A0ZZZL00?refer=kb_sec_share'
-    },
-    {
-      'name'  : '今日头条',
-      'image' : '../assets/img/logos/toutiao.png',
-      'link'  : 'https://www.toutiao.com/i6516421811100451331/?wxshare_count=2&pbid=6515233400638948878'
-    },
-    {
-      'name'  : '凤凰新闻',
-      'image' : '../assets/img/logos/ifeng.png',
-      'link'  : 'https://share.iclient.ifeng.com/news/shareNews?fromType=vampire&forward=1&aid=sub_47197129&ch=&aman=&gud=&ch=#backhead#backhead'
-    },
-    {
-      'name'  : '搜狐财经',
-      'image' : '../assets/img/logos/sohu.png',
-      'link'  : 'http://www.sohu.com/a/219659789_382178'
-    },
-    {
-      'name'  : '一点资讯',
-      'image' : '../assets/img/logos/yidianzixun.png',
-      'link'  : 'https://www.yidianzixun.com/article/0IFkhqYp?share_count=2'
-    },
-    {
-      'name'  : 'HackathonPost',
-      'image' : '../assets/img/logos/hackathonpost.png',
-      'link'  : 'http://hackathonpost.com/business/thebridgejp/7438'
-    },
-    {
-      'name'  : 'ICO Hikaku',
-      'image' : '../assets/img/logos/icohikaku.png',
-      'link'  : 'https://www.icohikaku.com/168927/'
-    }
-  ] 
+  // "member_of" : [
+  //   {
+  //     'name'  : 'SFA',
+  //     'image' : '../assets/img/logos/01_Logo_Original_Horizontal.png',
+  //     'link'  : 'https://singaporefintech.org/'
+  //   },
+  //   {
+  //     'name'  : 'ACCESS',
+  //     'image' : '../assets/img/logos/ACCESS-Crypto-Currency-Association-Singapore-1827259756.png',
+  //     'link'  : 'https://www.access-sg.org/'
+  //   }
+  // ],
+  // "featurings" : [
+  //   {
+  //     'name'  : 'Yahoo',
+  //     'image' : '../assets/img/logos/yahoo_logo_detail.png',
+  //     'link'  : 'https://sg.news.yahoo.com/blockchain-platform-startup-morpheus-labs-receives-angel-funding-082312233.html'
+  //   },
+  //   {
+  //     'name'  : 'e27',
+  //     'image' : '../assets/img/logos/e27-logo.png',
+  //     'link'  : 'https://e27.co/blockchain-startup-morpheus-labs-raises-funding-launches-ico-20180116/'
+  //   },
+  //   {
+  //     'name'  : 'Deal Street Asia',
+  //     'image' : '../assets/img/logos/dealstreet-720x720.png',
+  //     'link'  : 'https://www.dealstreetasia.com/stories/morpheus-labs-raises-funds-at-2m-valuation-25m-ico-90477/'
+  //   },
+  //   {
+  //     'name'  : 'Influencive',
+  //     'image' : '../assets/img/logos/influencive.png',
+  //     'link'  : 'https://www.influencive.com/morpheus-labs-potential-dominate-blockchain-era/'
+  //   },
+  //   {
+  //     'name'  : 'The Drum',
+  //     'image' : '../assets/img/logos/the_drum.png',
+  //     'link'  : 'http://www.thedrum.com/profile/news/281678/some-insight-morpheus-labs-potential-disrupt-the-blockchain-industry%201/30/178'
+  //   },
+  //   {
+  //     'name'  : '天天快报',
+  //     'image' : '../assets/img/logos/qq.png',
+  //     'link'  : 'https://kuaibao.qq.com/s/20180129A0ZZZL00?refer=kb_sec_share'
+  //   },
+  //   {
+  //     'name'  : '今日头条',
+  //     'image' : '../assets/img/logos/toutiao.png',
+  //     'link'  : 'https://www.toutiao.com/i6516421811100451331/?wxshare_count=2&pbid=6515233400638948878'
+  //   },
+  //   {
+  //     'name'  : '凤凰新闻',
+  //     'image' : '../assets/img/logos/ifeng.png',
+  //     'link'  : 'https://share.iclient.ifeng.com/news/shareNews?fromType=vampire&forward=1&aid=sub_47197129&ch=&aman=&gud=&ch=#backhead#backhead'
+  //   },
+  //   {
+  //     'name'  : '搜狐财经',
+  //     'image' : '../assets/img/logos/sohu.png',
+  //     'link'  : 'http://www.sohu.com/a/219659789_382178'
+  //   },
+  //   {
+  //     'name'  : '一点资讯',
+  //     'image' : '../assets/img/logos/yidianzixun.png',
+  //     'link'  : 'https://www.yidianzixun.com/article/0IFkhqYp?share_count=2'
+  //   },
+  //   {
+  //     'name'  : 'HackathonPost',
+  //     'image' : '../assets/img/logos/hackathonpost.png',
+  //     'link'  : 'http://hackathonpost.com/business/thebridgejp/7438'
+  //   },
+  //   {
+  //     'name'  : 'ICO Hikaku',
+  //     'image' : '../assets/img/logos/icohikaku.png',
+  //     'link'  : 'https://www.icohikaku.com/168927/'
+  //   }
+  // ] 
 };
