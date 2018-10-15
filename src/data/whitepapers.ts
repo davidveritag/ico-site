4ae987961e01013db12030b33eb694f7916f1208
kishan007 <kishansparkle007@gmail.com>
2018-10-16 00:18:41 +0530

    white paper is fixed

--- src/data/whitepapers.ts
+++ src/data/whitepapers.ts
@@ -5,14 +5,14 @@ export var Whitepapers = [
 	},
 	{
 		'lang' : '中文',
-		'link' : 'https://docs.google.com/document/d/1WUC1FjxPUZp-yfdJP_wdi2FMoo0KgpZcyi18PpCWzsw/edit?usp=sharing'
+		'link' : 'https://docs.google.com/document/d/1WUC1FjxPUZp-yfdJP_wdi2FMoo0KgpZcyi18PpCWzsw'
 	},
   {
 		'lang' : '日本語',
-		'link' : ''
+		'link' : 'https://docs.google.com/document/d/1WUC1FjxPUZp-yfdJP_wdi2FMoo0KgpZcyi18PpCWzsw'
 	},
   {
 		'lang' : '한국어',
-		'link' : ''
+		'link' : 'https://docs.google.com/document/d/1WUC1FjxPUZp-yfdJP_wdi2FMoo0KgpZcyi18PpCWzsw'
 	}
 ];
