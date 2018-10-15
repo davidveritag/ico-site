813c1ad707993e487d376c5f654d3553013468bb
kishan007 <kishansparkle007@gmail.com>
2018-10-16 00:20:54 +0530

    2

--- src/data/whitepapers.ts
+++ src/data/whitepapers.ts
@@ -3,10 +3,12 @@ export var Whitepapers = [
 		'lang' : 'English',
 		'link' : 'https://docs.google.com/document/d/1UyLUPFmdR1vRkDHH749alp8Y1GyyOgOInh9A3GKjIqk'
 	},
+
 	{
 		'lang' : '中文',
 		'link' : 'https://docs.google.com/document/d/1WUC1FjxPUZp-yfdJP_wdi2FMoo0KgpZcyi18PpCWzsw'
 	},
+	
   {
 		'lang' : '日本語',
 		'link' : 'https://docs.google.com/document/d/1WUC1FjxPUZp-yfdJP_wdi2FMoo0KgpZcyi18PpCWzsw'
