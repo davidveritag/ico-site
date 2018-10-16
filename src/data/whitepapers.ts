6739b1e77a67e0fe885b299b61afab55e6e2a5b8
kishan007 <kishansparkle007@gmail.com>
2018-10-16 11:54:04 +0530

    changes on 10/16/2018

--- src/data/whitepapers.ts
+++ src/data/whitepapers.ts
@@ -7,14 +7,14 @@ export var Whitepapers = [
 	{
 		'lang' : '中文',
 		'link' : 'https://docs.google.com/document/d/1WUC1FjxPUZp-yfdJP_wdi2FMoo0KgpZcyi18PpCWzsw'
-	},
-	
-  {
-		'lang' : '日本語',
-		'link' : 'https://docs.google.com/document/d/1WUC1FjxPUZp-yfdJP_wdi2FMoo0KgpZcyi18PpCWzsw'
-	},
-  {
-		'lang' : '한국어',
-		'link' : 'https://docs.google.com/document/d/1WUC1FjxPUZp-yfdJP_wdi2FMoo0KgpZcyi18PpCWzsw'
 	}
+	
+//   {
+// 		'lang' : '日本語',
+// 		'link' : 'https://docs.google.com/document/d/1WUC1FjxPUZp-yfdJP_wdi2FMoo0KgpZcyi18PpCWzsw'
+// 	},
+//   {
+// 		'lang' : '한국어',
+// 		'link' : 'https://docs.google.com/document/d/1WUC1FjxPUZp-yfdJP_wdi2FMoo0KgpZcyi18PpCWzsw'
+// 	}
 ];
