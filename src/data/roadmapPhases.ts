7539f67fd475eb98df95d73176a03131c36e1cf5
kishan007 <kishansparkle007@gmail.com>
2018-10-16 13:01:25 +0530

    roadmap redesigned

--- src/data/roadmapPhases.ts
+++ src/data/roadmapPhases.ts
@@ -4,10 +4,10 @@ export const techPhases: Phase[] = [
     new Phase('0','','','','past',true, false),
     new Phase('1','','','','current',false,false),
     new Phase('2','','','','future',false,false),
-    new Phase('3','','','','future',false,false),
-    new Phase('4','','','','future',false,false),
-    new Phase('5','','','','future',false,true),
-    new Phase('6','','','','future',false,true),
+    new Phase('3','','','','future',false,true),
+    // new Phase('4','','','','future',false,false),
+    // new Phase('5','','','','future',false,true),
+    // new Phase('6','','','','future',false,true),
 
   ]
 
@@ -16,11 +16,11 @@ export const techPhases: Phase[] = [
     new Phase('2','','','','past', false, false),
     new Phase('3','','','','current', false, false),
     new Phase('4','','','','future', false, false),
-    new Phase('5','','','','future', false, false),
-    new Phase('6','','','','future', false, true),
-    new Phase('7','','','','future', false, true),
-    new Phase('8','','','','future', false, true),
-    new Phase('9','','','','future', false, true),
-    new Phase('10','','','','future', false, true),
+    new Phase('5','','','','future', false, true),
+    // new Phase('6','','','','future', false, true),
+    // new Phase('7','','','','future', false, true),
+    // new Phase('8','','','','future', false, true),
+    // new Phase('9','','','','future', false, true),
+    // new Phase('10','','','','future', false, true),
 
   ]
