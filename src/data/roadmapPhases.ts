296cdcb46dbdd90b65ec5a0c5ef02349f68cd938
kishan <david@veritag.net>
2018-10-25 12:25:01 +0530

    roadmap status upto 2017

--- src/data/roadmapPhases.ts
+++ src/data/roadmapPhases.ts
@@ -2,8 +2,8 @@ import { Phase } from '../app/roadmap/phase/phase.model';
 
 export const techPhases: Phase[] = [
     new Phase('0','','','','past',true, false),
-    new Phase('1','','','','current',false,false),
-    new Phase('2','','','','future',false,false),
+    new Phase('1','','','','past',false,false),
+    new Phase('2','','','','current',false,false),
     new Phase('3','','','','future',false,true),
     // new Phase('4','','','','future',false,false),
     // new Phase('5','','','','future',false,true),
